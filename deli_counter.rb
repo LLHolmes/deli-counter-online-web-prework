186dfb3006a0b123ac0c11a231fbcafcc8e1a506
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-07 21:20:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,8 @@ def line (katz_deli)
 end
 
 def take_a_number (katz_deli)
-  katz_deli << 1
+  number = 1
+  katz_deli << number
   puts "Welcome, you are number #{number}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
